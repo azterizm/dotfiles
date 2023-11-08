8aeaebc4076163d7ada4520dc1d448ae5cb42fdd
azterizm <abdielprime@gmail.com>
2023-11-08 21:03:24 +0500

    nvchad

--- nvim.backup/after/plugin/colors.lua
+++
@@ -1,2 +0,0 @@
-vim.g.gruvbox_material_background = 'soft'
-vim.cmd [[ colorscheme gruvbox-material ]]
--- nvim.backup/after/plugin/indentline.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[highlight IndentBlanklineIndent guifg=#3b3b3b gui=nocombine]]
-
-require("indent_blankline").setup {
-  show_current_context = true,
-  show_current_context_start = false,
-  char_highlight_list = {
-    "IndentBlanklineIndent",
-  },
-}
